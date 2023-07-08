e498e2e248c65cbf292bd0d9c2828906a3445577
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2023-07-08 18:55:07 -0400

    Create game window

--- wc.py
+++ wc.py
@@ -1,1 +1,27 @@
-print('Hello World')
+import tkinter as tk
+
+def main():
+    mainWindow()
+
+def mainWindow():   
+    #make main window
+    mainWindow = tk.Tk()
+    mainWindow.title('Main Window')
+    mainWindow.geometry('500x500')
+    playButton = tk.Button(mainWindow, text='Play', command=lambda: [play(), mainWindow.destroy()])
+    playButton.pack()
+    seeStatsButton = tk.Button(mainWindow, text = 'See stats')
+    seeStatsButton.pack()
+    mainWindow.mainloop()
+
+
+def play():
+    #make game window
+    gameWindow = tk.Tk()
+    gameWindow.title('Game Window')
+    gameWindow.geometry('500x500')
+    
+
+
+main()
+
