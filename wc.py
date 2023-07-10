c87c75feb0e07b12720e0ad6367f136c7d107d28
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2023-07-09 20:54:10 -0400

    Add functionality for first row

--- wc.py
+++ wc.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 import random
 
+#intialize guess number to zero
+guesses = 0
+
 def main():
     #set up main window
     mainWindow()
@@ -19,20 +22,21 @@ def mainWindow():
 
 
 def play():
+
     #make game window
     gameWindow = tk.Tk()
     gameWindow.title('Game Window')
     gameWindow.geometry('500x500')
 
-    #intialize guess number to zero
-    guesses = 0
+
 
     #generate word to play with
     wordLine = random.randint(0, 2314)
     answersFile = open("/Users/carterwilliams/Coding_Projects/Wordle_Clone/wordle-clone/answers.txt", "r")
-    word = answersFile.readline()
+    word = answersFile.readline().strip()
     for x in range(wordLine):
-        word = answersFile.readline()
+        word = answersFile.readline().strip()
+    print(word)
 
     #create layout for game board
     r0c0 = tk.LabelFrame(gameWindow, height='40', width='40')
@@ -86,18 +90,44 @@ def play():
     r4c4 = tk.LabelFrame(gameWindow, height='40', width='40')
     r4c4.grid(row=4, column=4, padx='10', pady='10')
 
-    #Place guess box
+
+
+
+    def addGuess():
+        global guesses
+        guesses = guesses + 1
+        return guesses
+
+    def testGuess(guess, word, guesses):
+        guessList = list(guess)
+        wordList = list(word)
+        for x in range (0, 5):
+            if guesses == 1:
+                if guessList[x] == wordList[x]:
+                    if x == 0:
+                        r0c0.configure(bg='green')
+                    if x == 1:
+                        r0c1.configure(bg='green')
+                    if x == 2:
+                        r0c2.configure(bg='green')
+                    if x == 3:
+                        r0c3.configure(bg='green')
+                    if x == 4:
+                        r0c4.configure(bg='green')
+            
+
+    
+    #Place guess features
     guessLabelFrame = tk.LabelFrame(gameWindow)
     guessLabelFrame.grid(row=0, column=7)
     guessLabel = tk.Label(guessLabelFrame, text='Enter Guess Below:')
     guessLabel.pack()
     guessBox = tk.Entry(gameWindow)
     guessBox.grid(row=1, column=7)
+    print(guesses)
+    guessButton = tk.Button(gameWindow, text='SUBMIT', command = lambda: [testGuess(guessBox.get().lower(), word, addGuess())])
+    guessButton.grid(row = 2, column = 7)
+
 
     
-    
-    
-
-
 main()
-
